82b7efe41eba58c18bdf7991eaef61e803d8a125
nnhao14102000 <nnhao9a3@gmail.com>
2020-11-18 10:25:13 +0700

    Check Green and Red, chang factorial function, change yml

--- src/fu/util/MathUtility.java
+++ src/fu/util/MathUtility.java
@@ -10,24 +10,34 @@ package fu.util;
  * @author giao-lang
  */
 public class MathUtility {
+
     //ta sẽ làm class MathUtility giống y chang thiết kế của class Math
     //Math là class tiện ích 
     public static final double PI = 3.1415;
-    
+
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        if (n == 0 || n == 1)
+//            return 1; //đặc biệt, return luôn
+//        
+//        //đứa nào viết else ở đây, trừ điểm
+//        //mặc nhiên CPU đến đc đây là hiểu rằng n = 2..20
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+    //viet ham de quy cho pro
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20)
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be between 0..20");
-        if (n == 0 || n == 1)
-            return 1; //đặc biệt, return luôn
-        
-        //đứa nào viết else ở đây, trừ điểm
-        //mặc nhiên CPU đến đc đây là hiểu rằng n = 2..20
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
         }
-        return result;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n*getFactorial(n-1);
     }
-    
-    
+
 }
